722ed90309410fa46afb91cd017d60deb3135f78
shishir <shishirpatel@gmail.com>
2023-03-17 11:42:36 +0000

    initial design to sign-in page

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -50,7 +50,7 @@ const LoginForm = () => {
 							placeholder="Username"
 						/>
 					</div>
-				</div>
+	
 					<label className='sr-only'></label>
 						<input
 							className='relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
@@ -60,7 +60,7 @@ const LoginForm = () => {
 							onChange={(e) => setPassword(e.target.value)}
 							placeholder="Password"
 						/>
-
+					</div>
 				<div className="flex items-center justify-between">
 					<div className="flex items-center">
 						<input
